9e5ffc95c87b9ecfa4566d611823ddaf17613e0b
Nong Li <nong@cloudera.com>
2015-02-04 15:41:28 -0800

    Fix compiler issues from thrift renaming.
    The old classes are not getting cleaned up when we do the integration builds.
    
    Change-Id: Ie187665643a72b2fdc770ef5222d30cfae5d17e2
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5935
    Reviewed-by: Lenni Kuff <lskuff@cloudera.com>
    Tested-by: jenkins
    Reviewed-by: Nong Li <nong@cloudera.com>

--- record-service/java/sample/src/main/java/com/cloudera/recordservice/sample/SampleClient.java
+++ record-service/java/sample/src/main/java/com/cloudera/recordservice/sample/SampleClient.java
@@ -7,7 +7,6 @@ import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
-import com.cloudera.recordservice.thrift.RecordServiceException;
 import com.cloudera.recordservice.thrift.RecordServicePlanner;
 import com.cloudera.recordservice.thrift.RecordServiceWorker;
 import com.cloudera.recordservice.thrift.TExecTaskParams;
@@ -16,6 +15,7 @@ import com.cloudera.recordservice.thrift.TFetchParams;
 import com.cloudera.recordservice.thrift.TFetchResult;
 import com.cloudera.recordservice.thrift.TPlanRequestParams;
 import com.cloudera.recordservice.thrift.TPlanRequestResult;
+import com.cloudera.recordservice.thrift.TRecordServiceException;
 import com.cloudera.recordservice.thrift.TTask;
 
 /**
@@ -28,25 +28,25 @@ public class SampleClient {
   static final int WORKER_PORT = 40100;
 
   static TProtocol createConnection(int port, String serviceName)
-      throws TTransportException {
-    TTransport transport = new TSocket("localhost", port);
-    try {
-      transport.open();
-    } catch (TTransportException e) {
-      System.err.println("Could not connect to service: " + serviceName);
-      throw e;
+    throws TTransportException {
+      TTransport transport = new TSocket("localhost", port);
+      try {
+        transport.open();
+      } catch (TTransportException e) {
+        System.err.println("Could not connect to service: " + serviceName);
+        throw e;
+      }
+      return new TBinaryProtocol(transport);
     }
-    return new TBinaryProtocol(transport);
-  }
 
   public static void main(String[] args) throws TException {
-	  String query = DEFAULT_QUERY;
-	  if (args.length > 0) query = args[1];
+    String query = DEFAULT_QUERY;
+    if (args.length > 0) query = args[1];
 
-	  /**
-	   * First talk to the plan service to get the list of tasks.
-	   */
-	  System.out.println("Running request: " + query);
+    /**
+     * First talk to the plan service to get the list of tasks.
+     */
+    System.out.println("Running request: " + query);
 
     RecordServicePlanner.Client planner = new RecordServicePlanner.Client(
         createConnection(PLANNER_PORT, "Planner"));
@@ -54,20 +54,20 @@ public class SampleClient {
     try {
       TPlanRequestParams planParams = new TPlanRequestParams(query);
       planResult = planner.PlanRequest(planParams);
-    } catch (RecordServiceException e) {
+    } catch (TRecordServiceException e) {
       System.err.println("Could not plan request: " + e.message);
       throw e;
     } catch (TException e) {
       System.err.println("Could not plan request: " + e.getMessage());
       throw e;
     }
-	  System.out.println("Generated " + planResult.tasks.size() + " tasks.");
+    System.out.println("Generated " + planResult.tasks.size() + " tasks.");
 
 
-	  /**
-	   * Run each task on one of the workers.
-	   */
-	  int totalRows = 0;
+    /**
+     * Run each task on one of the workers.
+     */
+    int totalRows = 0;
     for (TTask task: planResult.tasks) {
       /* Start executing the task */
       RecordServiceWorker.Client worker = new RecordServiceWorker.Client(
@@ -76,7 +76,7 @@ public class SampleClient {
       try {
         TExecTaskParams taskParams = new TExecTaskParams(task.task);
         taskResult = worker.ExecTask(taskParams);
-      } catch (RecordServiceException e) {
+      } catch (TRecordServiceException e) {
         System.err.println("Could not exec task: " + e.message);
         throw e;
       } catch (TException e) {
@@ -92,7 +92,7 @@ public class SampleClient {
           fetchResult = worker.Fetch(fetchParams);
           totalRows += fetchResult.num_rows;
         } while (!fetchResult.done);
-      } catch (RecordServiceException e) {
+      } catch (TRecordServiceException e) {
         System.err.println("Could not fetch from task: " + e.message);
         throw e;
       } catch (TException e) {
