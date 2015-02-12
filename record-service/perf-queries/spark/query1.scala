165a14ead28f10a5425ddaa88996d5edb5346065
Nong Li <nong@cloudera.com>
2015-02-12 11:21:22 -0800

    Update steps to use spark and avro for perf queries.
    Change-Id: I27bbef49db93e66eddd9a3207004f5c6c1d6298e
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5990
    Tested-by: jenkins
    Reviewed-by: Nong Li <nong@cloudera.com>

--- record-service/perf-queries/spark/query1.scala
+++ record-service/perf-queries/spark/query1.scala
@@ -1,3 +1,8 @@
+# With RS
+val data = sc.recordServiceRecords("select l_partkey from tpch10gb_parquet.lineitem").
+    map(v => v(0).asInstanceOf[org.apache.hadoop.io.LongWritable].get()).
+    reduce(_ + _)
+
 # Run w/ Text:
 var path = "hdfs://localhost:20500/test-warehouse/tpch10gb.db/lineitem/*"
 var path = "hdfs://localhost:20500/test-warehouse/tpch.lineitem/*"
@@ -8,14 +13,22 @@ lineitem.map(line => line.split('|')(1).toLong).reduce(_ + _)
 # Run w/ Parquet:
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val data = sqlContext.parquetFile(path)
-
-v2:
 data.registerTempTable("lineitem")
 sqlContext.sql("select sum(l_partkey) from lineitem").collect()
 
+# Run w/ Avro (very painful):
+# Get and build spark-avro (patched)
+# https://github.com/nongli/spark-avro/tree/glob_fix
+# Get and build avro 1.7.8
 
-# With RS
-val data = sc.recordServiceRecords("select l_partkey from tpch10gb_parquet.lineitem").
-    map(v => v(0).asInstanceOf[org.apache.hadoop.io.LongWritable].get()).
-    reduce(_ + _)
+bin/spark-shell --jars <path to> avro-1.7.8-SNAPSHOT.jar,avro-mapred-1.7.8-SNAPSHOT.jar,spark-avro_2.10-0.1.jar
+import org.apache.spark.sql.SQLContext
+val sqlContext = new SQLContext(sc)
+import sqlContext._
+import com.databricks.spark.avro._
+val data = sqlContext.avroFile("/test-warehouse/tpch10gb_avro_snap.db/lineitem/*")
+
+# Note: spark sql appears to be case sensitive on identifiers
+data.registerTempTable("lineitem")
+sqlContext.sql("select sum(L_PARTKEY) from lineitem").collect()
 
