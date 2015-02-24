cd132d5d6a9305694cda78c0601ba77aa9dabfb1
Nong Li <nong@cloudera.com>
2015-02-23 18:55:56 -0800

    Fix instructions to run spark perf queries.
    Change-Id: I10ce6ca79490e8a628540c4016caacc6602e2ffa
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6047
    Tested-by: jenkins <cloudera-hudson@gerrit.cloudera.org>
    Reviewed-by: Lenni Kuff <lskuff@cloudera.com>

--- record-service/perf-queries/spark/query1.scala
+++ record-service/perf-queries/spark/query1.scala
@@ -1,4 +1,5 @@
 # With RS
+import com.cloudera.recordservice.spark._
 val data = sc.recordServiceRecords("select l_partkey from tpch10gb_parquet.lineitem").
     map(v => v(0).asInstanceOf[org.apache.hadoop.io.LongWritable].get()).
     reduce(_ + _)
@@ -11,6 +12,7 @@ var lineitem = sc.textFile(path)
 lineitem.map(line => line.split('|')(1).toLong).reduce(_ + _)
 
 # Run w/ Parquet:
+var path = "hdfs://localhost:20500/test-warehouse/tpch10gb_parquet.db/lineitem/*"
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val data = sqlContext.parquetFile(path)
 data.registerTempTable("lineitem")
