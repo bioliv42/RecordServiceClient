d380d6f3ca6d6eba3d1919dce183f084a68e8b35
Lenni Kuff <lskuff@cloudera.com>
2015-02-24 17:28:52 -0800

    Create 'redirect' table for running Hive benchmarks
    Change-Id: Ib245203764994376c16a8272c234d967f20ed869
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6052
    Reviewed-by: Nong Li <nong@cloudera.com>
    Tested-by: jenkins <cloudera-hudson@gerrit.cloudera.org>

--- record-service/tests/create-benchmark-tbls.sql
+++ record-service/tests/create-benchmark-tbls.sql
@@ -1,6 +1,7 @@
 -- Create databases
 CREATE DATABASE IF NOT EXISTS tpch6gb;
 CREATE DATABASE IF NOT EXISTS tpch6gb_parquet;
+CREATE DATABASE IF NOT EXISTS rs;
 
 -- Create tables
 DROP TABLE IF EXISTS tpch6gb.lineitem;
@@ -30,3 +31,10 @@ DROP TABLE IF EXISTS tpch6gb_parquet.lineitem;
 CREATE EXTERNAL TABLE tpch6gb_parquet.lineitem like tpch6gb.lineitem
 STORED AS PARQUET
 LOCATION '/test-warehouse/tpch6gb_parquet.lineitem';
+
+-- Table that is used for Hive tests and benchmarks. Until Hive has transparent support
+-- for RecordService SerDe, queries are executed against this table and then redirected
+-- to run against the actual db/table.
+DROP TABLE IF EXISTS rs.lineitem_hive_serde;
+CREATE EXTERNAL TABLE rs.lineitem_hive_serde like tpch6gb.lineitem
+STORED BY RECORDSERVICE;
