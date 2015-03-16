366c9e9d4f82622786695c7731333d7f829a43a7
Nong Li <nong@cloudera.com>
2015-03-16 14:36:11 -0700

    Add some jenkins build scripts.
    Change-Id: Idd8777c0d82318f29a5b54c0ac18be48fde4788a
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6180
    Reviewed-by: Nong Li <nong@cloudera.com>
    Tested-by: Nong Li <nong@cloudera.com>

---
+++ record-service/jenkins/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Builds impala, with output redirected to $WORKSPACE/buildall.log
+# TARGET_BUILD_TYPE can be set to Release/Debug
+
+pushd $IMPALA_HOME
+
+rm -f ./bin/version.info
+rm -f ./CMakeCache.txt
+
+echo
+echo "********************************************************************************"
+echo " Running buildall.sh "
+echo "********************************************************************************"
+echo
+
+if $CLEAN; then
+  echo ">>> Cleaning workspace"
+  git clean -dfx && git reset --hard HEAD
+  ./buildall.sh -clean -notests -skiptests -format_metastore\
+      >> $WORKSPACE/buildall.log 2>&1 || { echo "buildall.sh failed"; exit 1; }
+else
+  ./buildall.sh -noclean -notests -skiptests \
+      >> $WORKSPACE/buildall.log 2>&1 || { echo "buildall.sh failed"; exit 1; }
+fi
+
+popd
+
+echo
+echo "********************************************************************************"
+echo " buildall.sh succeeded"
+echo "********************************************************************************"
+echo
---
+++ record-service/jenkins/preamble.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Sets up a common environment for Jenkins builds.
+
+echo "********************************************************************************"
+echo " Building ${JOB_NAME} #${BUILD_NUMBER} "
+echo " Node: ${NODE_NAME} / `hostname`"
+echo " Branch: ${GIT_BRANCH}@${GIT_COMMIT}"
+echo " ${BUILD_URL}"
+echo " Path: ${WORKSPACE}"
+echo "********************************************************************************"
+
+# Find all java processes by this user that aren't slave.jar-related, and if
+# those processes' parents are 1, kill them to death.
+echo ">>> Killing left over processes"
+(ps -fe -u $USER |grep java|grep -v grep |grep -v "slave.jar" |awk '{ if ($3 ~ "1") { print $2 } }'|xargs kill -9)
+
+echo ">>> Setting up Jenkins environment..."
+echo ">>> Mounting toolchain"
+. /mnt/toolchain/toolchain.sh
+export IMPALA_HOME=$WORKSPACE/repos/Impala
+export IMPALA_LZO=$WORKSPACE/repos/Impala-lzo
+export HADOOP_LZO=$WORKSPACE/repos/hadoop-lzo
+
+unset THRIFT_HOME
+unset THRIFT_CONTRIB_DIR
+
+export LLVM_HOME=/opt/toolchain/llvm-3.3
+export PATH=$LLVM_HOME/bin:$PATH
+
+export MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=512m"
+
+export JAVA_HOME=$JAVA7_HOME
+export JAVA64_HOME=$JAVA7_HOME
+export PATH=$JAVA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=""
+export LD_PRELOAD=""
+# Re-source impala-config since JAVA_HOME has changed.
+cd $IMPALA_HOME
+. bin/impala-config.sh &> /dev/null
+
+# Enable core dumps
+ulimit -c unlimited
+
+echo ">>> Shell environment"
+env
+java -version
+ulimit -a
+
+echo
+echo
+echo "********************************************************************************"
+echo " Environment setup complete, build proper follows"
+echo "********************************************************************************"
+echo
+echo
+
