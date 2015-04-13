d4a192a53c7dab0f0c93a3f872123aecf8881d03
Nong Li <nong@cloudera.com>
2015-04-13 12:30:28 -0700

    Stop using Impala's thirdparty.

---
+++ thirdparty/download_thirdparty.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+
+TP_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+cd $TP_DIR
+
+source versions.sh
+
+if [ ! -d gflags-${GFLAGS_VERSION} ]; then
+  echo "Fetching gflags"
+  wget https://github.com/gflags/gflags/archive/v${GFLAGS_VERSION}.zip
+  mv v${GFLAGS_VERSION}.zip gflags-${GFLAGS_VERSION}.zip
+  unzip gflags-${GFLAGS_VERSION}.zip
+  rm gflags-${GFLAGS_VERSION}.zip
+fi
+
+if [ ! -d gtest-${GTEST_VERSION} ]; then
+  echo "Fetching gtest"
+  curl -OC - https://googletest.googlecode.com/files/gtest-${GTEST_VERSION}.zip
+  unzip gtest-${GTEST_VERSION}.zip
+  rm gtest-${GTEST_VERSION}.zip
+fi
