088ca9c523eba9528b4f5c7113b9e563b4211e23
Nong Li <nong@cloudera.com>
2015-07-28 10:33:00 -0700

    Build script clean up.
    Some left over artifacts when the scripts were split. Also added an option
    to skip building the java client (used for private cluster builds).
    
    Change-Id: I83272c803c92ce86f8977e0df50ddbcfe176dfe5
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7236
    Tested-by: jenkins
    Reviewed-by: Nong Li <nong@cloudera.com>

--- jenkins/build_impala.sh
+++ jenkins/build_impala.sh
@@ -9,6 +9,29 @@ echo "**************************************************************************
 echo " building RecordService daemons."
 echo "********************************************************************************"
 
+BUILD_ARGS="-notests -skiptests"
+
+# Get the HDFS version. If this changes, we need to format.
+HDFS_VERSION=`/bin/grep layoutVersion $CLUSTER_DIR/cdh5/node-1/data/dfs/nn/current/VERSION | cut -d= -f2`
+
+if [[ $CLEAN ]] && $CLEAN
+then
+  echo ">>> Cleaning workspace"
+  pushd $IMPALA_HOME
+  git clean -dfx && git reset --hard HEAD
+  BUILD_ARGS="$BUILD_ARGS -format_metastore"
+  popd
+fi
+
+# Version for CDH5.4 is -60
+if [ $HDFS_VERSION -ne -60 ]
+then
+  # This is a somewhat general hack for additional steps that need to be taken when
+  # upgrading CDH
+  echo ">>> Formatting HDFS"
+  BUILD_ARGS="$BUILD_ARGS -format"
+fi
+
 pushd $IMPALA_HOME
 rm -f ./bin/version.info
 rm -f ./CMakeCache.txt
--- jenkins/build_rs.sh
+++ jenkins/build_rs.sh
@@ -7,49 +7,24 @@
 
 echo
 echo "********************************************************************************"
-echo " Building clients."
+echo " building RecordService client."
 echo "********************************************************************************"
 echo
 
-BUILD_ARGS="-notests -skiptests"
-
-# Get the HDFS version. If this changes, we need to format. 
-HDFS_VERSION=`/bin/grep layoutVersion $CLUSTER_DIR/cdh5/node-1/data/dfs/nn/current/VERSION | cut -d= -f2`
-
-if [[ $CLEAN ]] && $CLEAN
-then
-  echo ">>> Cleaning workspace"
-  pushd $IMPALA_HOME
-  git clean -dfx && git reset --hard HEAD
-  BUILD_ARGS="$BUILD_ARGS -format_metastore"
-  popd
-fi
-
 pushd $RECORD_SERVICE_HOME
 git clean -dfx && git reset --hard HEAD
-popd
 
-# Version for CDH5.4 is -60
-if [ $HDFS_VERSION -ne -60 ]
-then
-  # This is a somewhat general hack for additional steps that need to be taken when
-  # upgrading CDH
-  pushd $RECORD_SERVICE_HOME
-  echo ">>> Formatting HDFS"
-  ./bin/build_thirdparty.sh
-  BUILD_ARGS="$BUILD_ARGS -format"
+./thirdparty/download_thirdparty.sh
+./thirdparty/build_thirdparty.sh
+
+cmake .
+make
+
+if [[ -z $RECORD_SERVICE_SKIP_JAVA_CLIENT ]]; then
+  pushd $RECORD_SERVICE_HOME/java
+  mvn package -DskipTests
   popd
 fi
 
-echo "********************************************************************************"
-echo " building RecordService client."
-echo "********************************************************************************"
-pushd $RECORD_SERVICE_HOME
-./thirdparty/download_thirdparty.sh
-./thirdparty/build_thirdparty.sh
-cmake .
-make
-pushd $RECORD_SERVICE_HOME/java
-mvn package -DskipTests
 popd
 
