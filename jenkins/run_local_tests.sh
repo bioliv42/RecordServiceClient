0324dd397703d4c0c6424e3a24aeb2826c99cac0
Alex Leblang <alex.leblang@cloudera.com>
2015-10-22 13:10:37 -0700

    Update Cluster Test Scripts
    This patch adds the ability to use sentry with the benchmark tests as well as
    with the s3 tests. This test also updates jenkins/run_local_tests.sh to give it
    an option to execute on s3.
    
    Change-Id: I37c9661c40af423bd2e964c67cfd8b173793edd6
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/8984
    Tested-by: jenkins
    Reviewed-by: Chao Sun <chao.sun@cloudera.com>

--- jenkins/run_local_tests.sh
+++ jenkins/run_local_tests.sh
@@ -3,6 +3,7 @@
 
 # Loads data and starts the cluster to run local tests
 
+export S3=${S3:-0}
 # Builds record service client.
 source $WORKSPACE/repos/RecordServiceClient/jenkins/preamble_rs.sh
 cd $RECORD_SERVICE_HOME
@@ -25,8 +26,16 @@ cd $IMPALA_HOME
 
 bin/start-impala-cluster.py -s 1 --start_recordservice --catalogd_args="-load_catalog_in_background=false"
 
-echo ">>> Loading test data"
-. $RECORD_SERVICE_HOME/tests/load-test-data.sh
-echo ">>> Running tests"
-. $RECORD_SERVICE_HOME/tests/run-all-tests.sh
+if [ $S3 -eq 1 ]; then
+  echo ">>> Loading test data"
+  . $RECORD_SERVICE_HOME/tests/load-test-data-s3.sh
 
+  echo ">>> Running tests"
+  cd $RECORD_SERVICE_HOME/java/core
+  mvn -Dtest=*S3* test
+else
+  echo ">>> Loading test data"
+  . $RECORD_SERVICE_HOME/tests/load-test-data.sh
+  echo ">>> Running tests"
+  . $RECORD_SERVICE_HOME/tests/run-all-tests.sh
+fi
