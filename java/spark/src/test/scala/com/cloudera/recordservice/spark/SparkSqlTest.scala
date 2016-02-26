560fc84fe72392b95e3996801eaf1d0c6548cab4
Chao Sun <sunchao.chris@gmail.com>
2016-02-26 13:05:45 -0800

    RS-116: Support char datatypes for Spark
    This adds support for char/varchar types in RecordServiceRelation.
    Also fixed a bug for Timestamp type.
    
    The patch also comes with simple tests for all the types supported.
    
    Change-Id: I31ddfb23d121ef83a21897a07a6856387e631621
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11467
    Tested-by: Jenkins User
    Reviewed-by: Chao Sun <chao.sun@cloudera.com>

--- java/spark/src/test/scala/com/cloudera/recordservice/spark/SparkSqlTest.scala
+++ java/spark/src/test/scala/com/cloudera/recordservice/spark/SparkSqlTest.scala
@@ -14,7 +14,11 @@
 
 package com.cloudera.recordservice.spark
 
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.types._
 import org.scalatest.{FunSuite}
 
 // TODO: more tests
@@ -45,6 +49,61 @@ class SparkSqlTest extends FunSuite with SharedSparkSQLContext {
     row = sc.sql("SELECT n_comment, n_name from nationTbl").collect()(5)
     assert(row.get(0) == "ven packages wake quickly. regu")
     assert(row.get(1) == "ETHIOPIA")
+  }
+
+  test("All Types Test") {
+    sc.sql(s"""
+              |CREATE TEMPORARY TABLE alltypestbl
+              |USING com.cloudera.recordservice.spark.DefaultSource
+              |OPTIONS (
+              |  RecordServiceTable 'rs.alltypes'
+              |)
+      """.stripMargin)
+
+    val rows = sc.sql("SELECT * FROM alltypestbl").collect()
+    assert(rows.length == 2)
+    val schema = rows(0).schema
+    assert(schema(0).dataType == BooleanType)
+    assert(schema(1).dataType == IntegerType)
+    assert(schema(2).dataType == IntegerType)
+    assert(schema(3).dataType == IntegerType)
+    assert(schema(4).dataType == LongType)
+    assert(schema(5).dataType == FloatType)
+    assert(schema(6).dataType == DoubleType)
+    assert(schema(7).dataType == StringType)
+    assert(schema(8).dataType == StringType)
+    assert(schema(9).dataType == StringType)
+    assert(schema(10).dataType == TimestampType)
+    assert(schema(11).dataType.isInstanceOf[DecimalType])
+
+    val formatter:SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    formatter.setTimeZone(TimeZone.getTimeZone("GMT"))
+
+    if (rows(0).getBoolean(0)) {
+      assert(rows(0).getInt(1) == 0)
+      assert(rows(0).getInt(2) == 1)
+      assert(rows(0).getInt(3) == 2)
+      assert(rows(0).getLong(4) == 3)
+      assert(rows(0).getFloat(5) == 4.0f)
+      assert(rows(0).getDouble(6) == 5.0)
+      assert(rows(0).getString(7) == "hello")
+      assert(rows(0).getString(8) == "vchar1")
+      assert(rows(0).getString(9) == "char1")
+      assert(formatter.format(rows(0).getTimestamp(10)) == "2015-01-01")
+      assert(rows(0).getDecimal(11).toString == "3.1415920000")
+    } else {
+      assert(rows(0).getInt(1) == 6)
+      assert(rows(0).getInt(2) == 7)
+      assert(rows(0).getInt(3) == 8)
+      assert(rows(0).getLong(4) == 9)
+      assert(rows(0).getFloat(5) == 10.0f)
+      assert(rows(0).getDouble(6) == 11.0)
+      assert(rows(0).getString(7) == "world")
+      assert(rows(0).getString(8) == "vchar2")
+      assert(rows(0).getString(9) == "char2")
+      assert(formatter.format(rows(0).getTimestamp(10)) == "2016-01-01")
+      assert(rows(0).getDecimal(11).toString == "1234.5678900000")
+    }
   }
 
   test("Predicate pushdown") {
