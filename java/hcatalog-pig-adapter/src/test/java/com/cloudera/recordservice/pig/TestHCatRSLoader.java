c89725a795ff8efaad67f3047cbdfb1bdf332cd0
Chao Sun <sunchao.chris@gmail.com>
2016-03-15 14:37:56 -0700

    Add simple test for SELECT query in HCatalog/Pig
    This adds a simple test for SELECT queries using HCatRSLoader
    with Pig.
    
    Note that we can't select some types such as smallint yet, and the
    schema for SELECT queries is not correct at the moment.
    We'll add/fix tests for this once these are resolved.
    
    Change-Id: I4e3cbd9b7aad9dc15ca541d2ee619f477af20867
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11766
    Tested-by: Jenkins User
    Reviewed-by: Li Li <lili@cloudera.com>

--- java/hcatalog-pig-adapter/src/test/java/com/cloudera/recordservice/pig/TestHCatRSLoader.java
+++ java/hcatalog-pig-adapter/src/test/java/com/cloudera/recordservice/pig/TestHCatRSLoader.java
@@ -19,17 +19,22 @@
 
 package com.cloudera.recordservice.pig;
 
-import org.apache.pig.ExecType;
-import org.apache.pig.PigServer;
-import org.apache.pig.backend.executionengine.ExecException;
-import org.apache.pig.data.Tuple;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
+
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 // TODO: add more tests
 public class TestHCatRSLoader {
@@ -47,18 +52,69 @@ public class TestHCatRSLoader {
 
   @Test
   public void testNation() throws IOException {
-    server.registerQuery(
-        "A = LOAD 'tpch.nation' USING com.cloudera.recordservice.pig.HCatRSLoader();");
+    server.registerQuery("A = LOAD " +
+        "'tpch.nation' USING com.cloudera.recordservice.pig.HCatRSLoader();");
+
+    Schema s = server.dumpSchema("A");
+    List<Schema.FieldSchema> fields = s.getFields();
+    assertEquals(4, fields.size());
+    assertEquals("n_nationkey", fields.get(0).alias);
+    assertEquals(DataType.INTEGER, fields.get(0).type);
+    assertEquals("n_name", fields.get(1).alias);
+    assertEquals(DataType.CHARARRAY, fields.get(1).type);
+    assertEquals("n_regionkey", fields.get(2).alias);
+    assertEquals(DataType.INTEGER, fields.get(2).type);
+    assertEquals("n_comment", fields.get(3).alias);
+    assertEquals(DataType.CHARARRAY, fields.get(3).type);
+
     server.registerQuery("A_GROUP = GROUP A ALL;");
     server.registerQuery("A_COUNT = FOREACH A_GROUP GENERATE COUNT(A);");
     Iterator<Tuple> it = server.openIterator("A_COUNT");
     boolean first = true;
     while (it.hasNext()) {
-      Assert.assertTrue("Result should only contain one row", first);
+      assertTrue("Result should only contain one row", first);
       first = false;
       Tuple t = it.next();
-      Assert.assertEquals(1, t.size());
-      Assert.assertEquals(25L, t.get(0));
+      assertEquals(DataType.LONG, t.getType(0));
+      assertEquals(1, t.size());
+      assertEquals(25L, t.get(0));
     }
   }
+
+  @Test
+  // TODO: can't select n_nationkey due to RS-134. Add test once that is resolved.
+  public void testNationSelect() throws IOException {
+    server.registerQuery("A = LOAD 'select n_name, n_comment from tpch.nation'" +
+        " USING com.cloudera.recordservice.pig.HCatRSLoader();");
+    Schema s = server.dumpSchema("A");
+    // TODO: note: there's a bug in HCatRSLoader#getSchema, which returns the
+    // schema for the whole table instead of the selected columns. Fix the test
+    // after that is resolved.
+    List<Schema.FieldSchema> fields = s.getFields();
+    assertEquals(4, fields.size());
+    assertEquals("n_nationkey", fields.get(0).alias);
+    assertEquals(DataType.INTEGER, fields.get(0).type);
+    assertEquals("n_name", fields.get(1).alias);
+    assertEquals(DataType.CHARARRAY, fields.get(1).type);
+    assertEquals("n_regionkey", fields.get(2).alias);
+    assertEquals(DataType.INTEGER, fields.get(2).type);
+    assertEquals("n_comment", fields.get(3).alias);
+    assertEquals(DataType.CHARARRAY, fields.get(3).type);
+
+    Iterator<Tuple> it = server.openIterator("A");
+    int count = 0;
+    while (it.hasNext()) {
+      count++;
+      Tuple t = it.next();
+      assertEquals(2, t.size());
+      assertEquals(DataType.CHARARRAY, t.getType(0));
+      assertEquals(DataType.CHARARRAY, t.getType(1));
+      if (count == 1) {
+        assertEquals("ALGERIA", t.get(0));
+        assertEquals(" haggle. carefully final deposits detect slyly agai", t.get(1));
+      }
+    }
+    assertEquals(25L, count);
+  }
+
 }
